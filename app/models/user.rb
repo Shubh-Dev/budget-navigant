3a01fdd4b763b5344a932d6965bfeebbc2a40fbf
Shubh <shubh.scb@gmail.com>
2022-11-15 00:03:51 +0530

    GROUP TABLE CREATE

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # validates
+
+  validates :name , presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
+
+end
