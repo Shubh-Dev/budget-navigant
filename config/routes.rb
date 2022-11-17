a4f59133cc42e1a63ce9adc0ba7df661e5ff86f1
Shubh <shubh.scb@gmail.com>
2022-11-17 16:20:11 +0530

    PROGRESS

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :groups, only: [:index, :show, :new, :create, :destroy ] 
   resources :payments, only: [:index, :show, :new, :create, :destroy ]
-  
+
+  authenticated :user do
+    root :to => 'groups#index', as: :authenticated_root
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "groups#index"
+  root to: "splash#index"
 end
